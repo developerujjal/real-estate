0ede03494a93ed7a8c0403dbe1e8a67902f0929d
Developer Ujjal <ujjalchandra213@gmail.com>
2024-11-15 02:22:08 +0600

    completed

--- src/firebaseConfig/firebase.config.js
+++ src/firebaseConfig/firebase.config.js
@@ -1,23 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-/* const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEbUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER,
-  appId: import.meta.env.VITE_APPID
-};
- */
-
 const firebaseConfig = {
-  apiKey: "AIzaSyB9IHn_QtF0Y34GXlljvMdlLdbEVEO1XW4",
-  authDomain: "dragon-news-fc225.firebaseapp.com",
-  projectId: "dragon-news-fc225",
-  storageBucket: "dragon-news-fc225.firebasestorage.app",
-  messagingSenderId: "1087613195523",
-  appId: "1:1087613195523:web:8bdab831c438a78f394536"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 
